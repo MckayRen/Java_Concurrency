031d2cdc3fef858321a568792f924ed0bcf56fa3
Mckay <729022340@qq.com>
2018-10-07 17:12:59 +0800

    ❌UncaughtExceptionHandler

--- src/main/java/me/renkai/concurrency/InterruptDemo.java
+++ src/main/java/me/renkai/concurrency/InterruptDemo.java
@@ -1,18 +1,27 @@
 package me.renkai.concurrency;
 
 public class InterruptDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Thread thread = new Thread(() -> {
             try {
                 Thread.sleep(3000);
             } catch (InterruptedException e) {
-                System.out.println(Thread.currentThread().isInterrupted());
+                System.out.println("thread1 " + Thread.currentThread().isInterrupted());
                 Thread.currentThread().interrupt();
-                System.out.println(Thread.currentThread().isInterrupted());
+                System.out.println("thread2 " + Thread.currentThread().isInterrupted());
+                System.out.println(1/0);
             }
         });
 
+        thread.setUncaughtExceptionHandler((Thread t, Throwable e)
+                -> System.out.println(t + "  " + e));
+
         thread.start();
+        System.out.println("main1 " + thread.isInterrupted());
+        Thread.sleep(1000);
+        System.out.println("main2 " + thread.isInterrupted());
         thread.interrupt();
+        System.out.println("main3 " + thread.isInterrupted());
+
     }
 }
