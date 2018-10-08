78a610c6f8d86dd7cfda779942b021fc5186a12c
Mckay <729022340@qq.com>
2018-10-08 22:24:32 +0800

    返回json

--- src/main/java/me/renkai/rest/FirstRest.java
+++ src/main/java/me/renkai/rest/FirstRest.java
@@ -6,8 +6,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FirstRest {
 
+    private class Body {
+        private String message;
+
+        Body(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+    }
     @GetMapping("/hello")
-    public String first() {
-        return "hello world!";
+    public Body first() {
+        return new Body("hello world!");
     }
 }
