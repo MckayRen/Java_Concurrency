d8753e31b82d77fba47189a824812127940d8921
Mckay <729022340@qq.com>
2019-04-28 21:30:14 +0800

    Practice

--- src/test/java/me/renkai/practice/MainApplicationTests.java
+++ src/test/java/me/renkai/practice/MainApplicationTests.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
